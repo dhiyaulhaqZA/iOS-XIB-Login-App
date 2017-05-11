69b7e3370bbd41d7bf31a3efa718ddb5c154e26e
dhiyaulhaqZA <dhiya.alamsyah@students.amikom.ac.id>
2017-05-12 00:01:27 +0700

    Initial Commit

---
+++ XIB Login App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XIB Login App
+//
+//  Created by Dhiya Ulhaq Zulha Alamsyah on 5/12/17.
+//  Copyright © 2017 Dhiya Ulhaq Zulha Alamsyah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
